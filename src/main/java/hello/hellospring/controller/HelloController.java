af339d745323c178696ad4407773999d96775e03
jingyu9804 <swc05161@gmail.com>
2022-12-09 19:56:54 +0900

    feat(hellospring) : 회원 리포지토리 메모리 구현체 생성

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class  HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
