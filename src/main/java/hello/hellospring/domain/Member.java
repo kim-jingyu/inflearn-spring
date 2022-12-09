bb64cea9553138b610803ef4884621fef35a9d41
jingyu9804 <swc05161@gmail.com>
2022-12-09 19:13:04 +0900

    feat(hellospring) : 회원 도메인 객체 생성

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long id;    //회원 ID - 임의의 값
+    private String name;    //이름
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
