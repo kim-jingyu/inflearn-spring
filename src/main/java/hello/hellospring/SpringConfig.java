0191954c0f01d4b018484b9996fc7ba5eff2e32b
jingyu9804 <swc05161@gmail.com>
2022-12-18 17:46:44 +0900

    feat(hellospring) : 스프링 데이터 JPA 회원 리포지토를 사용하도록 스프링 설정 변경

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -14,15 +14,30 @@ import javax.sql.DataSource;
 public class SpringConfig {
 
     /**
+     * SpringDataJpa가 만들어놓은 구현체가 등록이 된다. -> memberRepository
+     * 그리고 memberService에 의존 관계를 세팅해줘야 함
+     * 그러면 스프링 컨테이너에서 memberRepository를 찾음 근데 내가 등록해놓은게 없음
+     * -> SpringDataJpa에서 interface만 만들어놓으면 구현체를 만들어놓고 스프링 빈에 등록함
+     * 그래서 우리는 인젝션을 받을 수 있음 -> memberService 등록하면 됨
+     */
+    private final MemberRepository memberRepository;
+
+//    생성자가 하나면 @Autowired 어노테이션 생략 가능
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
      * EntityManager가 필요함
      */
 //    @PersistenceContext // 스펙에서는 이렇게 해야함.
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     /**
      * DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다
@@ -37,16 +52,20 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
+    /**
+     * 스프링 데이터 jpa 사용시, 인젝션 받은 것을 memberService에 의존관계 세팅
+     * memberRepository() -> memberRepository
+     */
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
