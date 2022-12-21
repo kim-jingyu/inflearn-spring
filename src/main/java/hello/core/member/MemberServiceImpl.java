e6541b89435eb328844e5ac449ca393a1d610364
jingyu9804 <swc05161@gmail.com>
2022-12-22 00:54:40 +0900

    refactor(core) : 변수 범위 재설정

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    private static MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
     public void join(Member member) {
