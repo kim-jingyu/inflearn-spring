7ff8df455b9ff4abc2ce8cf45d859ba1ee025e83
jingyu9804 <swc05161@gmail.com>
2022-12-23 19:10:57 +0900

    test(core) : 스프링 컨테이너에서 스프링 빈을 찾는 가장 기본적인 조회 방법

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicInfoTest.java
@@ -0,0 +1,46 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextBasicInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("이름없이 타입만으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);  //같은 타입이 여러 개일 경우는 곤란해질 수 있음.
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByNameWithImpl() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class); // 구체 타입으로 조회하면 변경시 유연성이 떨어진다.
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 안될때")
+    void findBeanByNameX() {˚
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("hi", MemberService.class));
+    }
+
+}
