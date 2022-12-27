50db522a9a89c7b568bb2c580a5e8bbda84a5ca1
jingyu9804 <swc05161@gmail.com>
2022-12-27 15:40:43 +0900

    feat(core) : 롬복 라이브러리가 제공하는 @RequiredArgsConstructor 기능을 사용하여 final이 붙은 필드를 모아서 생성자 자동으로 만들어주기.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,10 +5,12 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     /**
@@ -39,10 +41,10 @@ public class OrderServiceImpl implements OrderService{
     }*/
 
     //생성자 주입
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    /*public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
