23a81ecacc4ad94f3aacf7ebe06df781b83751c3
jingyu9804 <swc05161@gmail.com>
2022-12-28 22:22:56 +0900

    test(core) : 빈 스코프 - 싱글톤타입 빈 스코프과 프로토타입 빈 스코프 비교 테스트

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,42 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+
+    @Test
+    public void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Singleton.class);
+
+        Singleton bean1 = ac.getBean(Singleton.class);
+        Singleton bean2 = ac.getBean(Singleton.class);
+
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+
+        Assertions.assertThat(bean1).isSameAs(bean2);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class Singleton {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("Singleton.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("Singleton.destroy");
+        }
+    }
+}
