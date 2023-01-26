56bde1d4641ca56f14757ad045272e8124053a7b
jingyu9804 <swc05161@gmail.com>
2023-01-26 16:07:52 +0900

    test(scope) : 싱글톤 빈과 프로토타입 스코프 빈을 함께 사용시 문제가 발생 테스트

---
+++ src/test/java/practice/core2/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,62 @@
+package practice.core2.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * 싱글톤 빈과 프로토타입 스코프 빈을 함께 사용시 문제가 발생한다.
+ */
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        System.out.println("count1 = " + count1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        System.out.println("count2 = " + count2);
+    }
+
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("PrototypeBean.close " + this);
+        }
+    }
+}
