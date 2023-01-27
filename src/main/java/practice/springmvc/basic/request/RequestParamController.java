fe10e0027b897670d6f967b65a91d22f6554c8bc
jingyu9804 <swc05161@gmail.com>
2023-01-27 19:44:58 +0900

    html(practice) : HTTP 요청 파라미터 조회 - @RequestParam

--- src/main/java/practice/springmvc/basic/request/RequestParamController.java
+++ src/main/java/practice/springmvc/basic/request/RequestParamController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -22,5 +24,27 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    /**
+     * 파라미터 이름으로 바인딩
+     *
+     * @param memberName
+     * @param memberAge
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                                 @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username, @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
     }
 }
