0fe9a2085e2d12ec0e2d4a3e9e2f9400c15aa5f1
jingyu9804 <swc05161@gmail.com>
2023-01-27 22:55:00 +0900

    feat(practice) : HTTP 요청 파라미터 조회 - @ModelAttribute argument resolver 로 지정해둔 타입 외 나머지

--- src/main/java/practice/springmvc/basic/request/RequestParamController.java
+++ src/main/java/practice/springmvc/basic/request/RequestParamController.java
@@ -3,12 +3,12 @@ package practice.springmvc.basic.request;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import practice.springmvc.basic.HelloData;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * 요청 파라미터 조회
@@ -63,4 +63,26 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
         return "ok";
     }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam MultiValueMap<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
 }
