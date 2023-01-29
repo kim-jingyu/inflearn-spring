8e15c36301fe2c3aad0d5a27d298e0f513c63ec7
jingyu9804 <swc05161@gmail.com>
2023-01-30 00:03:37 +0900

    feat(item-service) : 상품 저장소

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,43 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 상품 저장소
+ */
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item saveItem(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findItemById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAllItems() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void updateItem(Long itemId, Item paramItem) {
+        Item foundItem = findItemById(itemId);
+        foundItem.setItemName(paramItem.getItemName());
+        foundItem.setPrice(paramItem.getPrice());
+        foundItem.setQuantity(paramItem.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
