b35895fafed53d9b1088dddcd20e4979cb144fd7
jingyu9804 <swc05161@gmail.com>
2023-02-12 18:47:43 +0900

    feat(item-service) : WebConfig - LoginCheckFilter 추가

--- src/main/java/hello/itemservice/WebConfig.java
+++ src/main/java/hello/itemservice/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
 import hello.itemservice.web.filter.LogFilter;
+import hello.itemservice.web.filter.LoginCheckFilter;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +27,20 @@ public class WebConfig {
         // 필터 체인 순서
         filterRegistrationBean.setOrder(1);
         // 필터를 적용할 URL 패턴 지정, 한번에 여러 패턴 지정 가능
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        // 로그인 필터 등록
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        // 필터 체인 순서
+        filterRegistrationBean.setOrder(2);
+        // 모든 요청에 로그인 필터 적용 -> 구현 필터 내부에서 whitelist 로 제외
         filterRegistrationBean.addUrlPatterns("/*");
 
         return filterRegistrationBean;
