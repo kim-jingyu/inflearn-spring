c1a4ebbda0376ed7d4137537194d75b3df9738b6
jingyu9804 <swc05161@gmail.com>
2023-02-12 18:17:03 +0900

    feat(item-service) : WebConfig 생성

---
+++ src/main/java/hello/itemservice/WebConfig.java
@@ -0,0 +1,33 @@
+package hello.itemservice;
+
+import hello.itemservice.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 필터 설정
+ */
+@Configuration
+public class WebConfig {
+
+    /**
+     * 필터를 등록하는 방법
+     * 스프링 부트를 사용한다면 FilterRegistrationBean 을 사용해서 등록
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        // 등록할 필터 지정
+        filterRegistrationBean.setFilter(new LogFilter());
+        // 필터 체인 순서
+        filterRegistrationBean.setOrder(1);
+        // 필터를 적용할 URL 패턴 지정, 한번에 여러 패턴 지정 가능
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
