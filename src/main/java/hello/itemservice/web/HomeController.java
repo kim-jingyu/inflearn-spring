c6aa46b56c645765dcb4c6e1e741341816b6ee44
jingyu9804 <swc05161@gmail.com>
2023-02-12 16:21:17 +0900

    feat(item-service) : @SessionAttribute 어노테이션 사용하기

--- src/main/java/hello/itemservice/web/HomeController.java
+++ src/main/java/hello/itemservice/web/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -72,7 +73,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping
+//    @GetMapping
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         HttpSession session = request.getSession(false);
@@ -90,6 +91,22 @@ public class HomeController {
 
         // 세션이 유지되면 로그인으로 이동
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    /**
+     * 스프링은 세션을 더 편리하게 사용할 수 있도록 @SessionAttribute 를 지원한다.
+     */
+    @GetMapping
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 세션에 회원 데이터가 없으면 기본 화면 반환
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
