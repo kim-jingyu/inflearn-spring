01c2a33a5b3743f71654e87c45e281fc80aaa85f
jingyu9804 <swc05161@gmail.com>
2023-02-13 17:01:20 +0900

    feat(item-service) : ArgumentResolver 를 사용해서 편리하게 로그인 회원 정보를 조회 - WebConfig 에 설정 추가

--- src/main/java/hello/itemservice/WebConfig.java
+++ src/main/java/hello/itemservice/WebConfig.java
@@ -1,5 +1,6 @@
 package hello.itemservice;
 
+import hello.itemservice.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.itemservice.web.filter.LogFilter;
 import hello.itemservice.web.filter.LoginCheckFilter;
 import hello.itemservice.web.interceptor.LogInterceptor;
@@ -8,14 +9,22 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * 필터 설정
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
