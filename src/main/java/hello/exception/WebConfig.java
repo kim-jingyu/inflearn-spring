06bd25e566bb735d1479e08f6aee5ef1943eb00a
jingyu9804 <swc05161@gmail.com>
2023-02-15 20:53:39 +0900

    feat(exception) : MyHandlerExceptionResolver 등록

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -2,20 +2,37 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 /**
- * 필터 등록
+ * 필터, 인터셉터 등록
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * MyHandlerExceptionResolver 등록
+     * @param resolvers the list of configured resolvers to extend
+     */
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
+
+    /**
+     * 로그 인터셉터 등록
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor())
