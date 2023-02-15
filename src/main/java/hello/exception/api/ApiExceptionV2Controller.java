3304b000736203cf8a4d7ce716823d4c8d294a5b
jingyu9804 <swc05161@gmail.com>
2023-02-16 03:36:25 +0900

    feat(exception): @ExceptionHandler 예외 처리 연습 컨트롤러

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,11 +1,13 @@
 package hello.exception.api;
 
 import hello.exception.result.ErrorResult;
+import hello.exception.userdefinition.UserException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -13,8 +15,48 @@ public class ApiExceptionV2Controller {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalArgExHandle(IllegalArgumentException e) {
-        log.error("IllegalArg Exception 예외 발생!!", e);
-        return new ErrorResult("BAD REQUEST", e.getMessage());
+    public ErrorResult illegalExHandle(IllegalArgumentException e) {
+        log.error("[ExceptionHandler] Illegal EX!!", e);
+        return new ErrorResult("BAD-REQUEST", e.getMessage());
     }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
+        log.error("[ExceptionHandler] User EX!!", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorResult exceptionHandle(Exception e) {
+        log.error("[ExceptionHandler] Exception!!", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
+    @GetMapping("/api2/members/{id}")
+    public MemberDto getMember(@PathVariable String id) {
+
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+
 }
