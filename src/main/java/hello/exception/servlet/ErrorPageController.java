172fb497aa0474bddfbd13330d29e85494a6aab8
jingyu9804 <swc05161@gmail.com>
2023-02-15 17:19:04 +0900

    feat(exception): API 응답 추가

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,10 +1,18 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 오류 페이지를 보여주기 위한 컨트롤러 ( 오류 출력 )
@@ -35,6 +43,21 @@ public class ErrorPageController {
         log.info("500 오류 페이지를 보여주기 위한 컨트롤러 호출");
         printErrorInfo(request);
         return "error-page/500";
+    }
+
+    // 클라이언트가 받고 싶은 미디어타입(Accept) 가 json 이면 이 컨트롤러의 메서드가 호출됨.
+    @RequestMapping(value = "/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
+        log.info("API JSON 500 오류 페이지를 보여주기 위한 컨트롤러 호출");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        Integer statusCode = (Integer) request.getAttribute(ERROR_STATUS_CODE);
+
+        result.put("status", statusCode);
+        result.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
     }
 
     // request.attribute 에 WAS 서버가 담아준 정보 출력
