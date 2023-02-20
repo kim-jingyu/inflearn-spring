f327b40fe3e9e236961125a9c623fe3c15738a87
jingyu9804 <swc05161@gmail.com>
2023-02-20 20:59:28 +0900

    feat(jdbc) : DriverManger.getConnection - JDBC

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -8,16 +8,25 @@ import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
+/**
+ * JDBC 사용해서 DB에 연결
+ */
 @Slf4j
 public class DBConnectionUtil {
 
     public static Connection getConnection() {
         try {
+            // DB에 연결하려면 JDBC 가 제공하는 DriverManager.getConnection 을 사용하면 된다.
+            // 라이브러리에 있는 DB 드라이버를 찾아서 실제 DB와 connection 을 맺고
+            // 해당 드라이버가 제공하는 connection 을 반환해준다.
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            log.info("Connection = {}, connection.getClass() = {}", connection, connection.getClass());
+
+            // class org.h2.jdbc.JdbcConnection : H2 데이터베이스 드라이버가 제공하는 H2 전용 Connection
+            log.info("connection 정보 = {}, 클래스 = {}", connection, connection.getClass());
             return connection;
         } catch (SQLException e) {
             throw new IllegalStateException(e);
         }
     }
+
 }
