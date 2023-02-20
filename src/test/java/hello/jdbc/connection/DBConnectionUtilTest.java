24512b77a3ff2c897f8904b2e7de680fba337484
jingyu9804 <swc05161@gmail.com>
2023-02-20 17:05:42 +0900

    feat(jdbc) : DBConnectionUtil

---
+++ src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,18 @@
+package hello.jdbc.connection;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+    }
+}
