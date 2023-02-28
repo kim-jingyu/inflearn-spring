f2e837e6ae50aa0929628383e076c53cb65a69ea
jingyu9804 <swc05161@gmail.com>
2023-03-01 00:05:20 +0900

    test(jdbc) : 체크 예외의 문제점 테스트

---
+++ src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -0,0 +1,46 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+/**
+ * 체크 예외의 문제점
+ * 1. 복구 불가능한 예외
+ * 2. 의존 관계에 대한 문제
+ */
+@Slf4j
+public class CheckedAppTest {
+
+    static class NetworkClient {
+        public void call() throws ConnectException {
+            throw new ConnectException("ConnectException 연결 실패!!");
+        }
+    }
+
+    static class Repository {
+        public void call() throws SQLException {
+            throw new SQLException("SQLException DB 에러!!");
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() throws ConnectException, SQLException {
+            networkClient.call();
+            repository.call();
+        }
+    }
+
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+}
