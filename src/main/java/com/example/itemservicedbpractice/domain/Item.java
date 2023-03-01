bf3d2d41280ff942f6f454be3b53275e5ea593bc
jingyu9804 <swc05161@gmail.com>
2023-03-02 01:24:09 +0900

    feat(item-service-db) : 상품 객체

--- src/main/java/com/example/itemservicedbpractice/domain/Item.java
+++ src/main/java/com/example/itemservicedbpractice/domain/Item.java
@@ -1,25 +1,15 @@
 package com.example.itemservicedbpractice.domain;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 /**
  * 상품 자체를 나타내는 객체
  */
 @Getter @Setter @ToString @EqualsAndHashCode
+@NoArgsConstructor @AllArgsConstructor
 public class Item {
+    private Long id; // 상품 번호 (key)
     private String itemName; // 상품 이름
     private Integer price; // 상품 가격
     private Integer quantity; // 상품 수량
-
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
