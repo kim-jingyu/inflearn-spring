1cbd92db6199971d62de889617c58e9fdbb7fa0e
jingyu9804 <swc05161@gmail.com>
2023-03-01 17:53:25 +0900

    feat(jdbc) : 회원 리포지토리 인터페이스 - 런타임 예외 변환 적용

--- src/main/java/hello/jdbc/repository/MemberRepository.java
+++ src/main/java/hello/jdbc/repository/MemberRepository.java
@@ -3,10 +3,8 @@ package hello.jdbc.repository;
 import hello.jdbc.domain.Member;
 
 public interface MemberRepository {
-    public Member save(Member member);
-    public Member findMember(String memberId);
-
-    public void updateMember(String memberId, int money);
-
-    public void deleteMember(String memberId);
+    Member save(Member member);
+    Member findMember(String memberId);
+    void updateMember(String memberId, int money);
+    void deleteMember(String memberId);
 }
