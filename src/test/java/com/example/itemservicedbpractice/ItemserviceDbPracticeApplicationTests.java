4dd7d63706b5383e888a83aa41ca8e977df0efc3
jingyu9804 <swc05161@gmail.com>
2023-03-02 01:16:32 +0900

    Initial commit

---
+++ src/test/java/com/example/itemservicedbpractice/ItemserviceDbPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemservicedbpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemserviceDbPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
