63c41a1e3e185b87b014da5b3ec54089be982166
jingyu9804 <swc05161@gmail.com>
2023-03-03 19:14:00 +0900

    feat(itemservice-db) : 데이터 접근 기술 - 스프링 JdbcTemplate

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -5,10 +5,17 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,22 +31,82 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
     @Override
     public Item save(Item item) {
         String sql = "insert into item values(?,?,?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(connection -> {
+            // id -> 자동 증가 키 지정
+            PreparedStatement psmt = connection.prepareStatement(sql, new String[]{"id"});
+            psmt.setString(1, item.getItemName());
+            psmt.setInt(2, item.getPrice());
+            psmt.setInt(3, item.getQuantity());
+            return psmt;
+        }, keyHolder);
 
-        return null;
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+        return item;
     }
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item set item_name = ?, price = ?, quantity = ? where id = ?";
 
+        template.update(sql, updateParam.getItemName(), updateParam.getPrice(), updateParam.getQuantity(), itemId);
     }
 
     @Override
     public Optional<Item> findById(Long id) {
-        return Optional.empty();
+        String sql = "select * from item where id = ?";
+
+        try {
+            Item item = template.queryForObject(sql, itemRowMapper(), id);
+            return Optional.of(item);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        String sql = "select * from item";
+
+        //동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+
+            boolean andFlag = false;
+            List<Object> param = new ArrayList<>();
+            if (StringUtils.hasText(itemName)) {
+                sql += " item_name like concat('%',?,'%')";
+                param.add(itemName);
+                andFlag = true;
+            }
+
+            if (maxPrice != null) {
+                if (andFlag) {
+                    sql += " and";
+                }
+                sql += " price <= ?";
+                param.add(maxPrice);
+            }
+
+            log.info("SQL 문 = {}", sql);
+            return template.query(sql, itemRowMapper(), param.toArray());
+        }
+
         return null;
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return ((rs, rowNum) -> {
+            Item item = new Item();
+            item.setId(rs.getLong("id"));
+            item.setItemName(rs.getString("item_name"));
+            item.setPrice(rs.getInt("price"));
+            item.setQuantity(rs.getInt("quantity"));
+            return item;
+        });
     }
 }
