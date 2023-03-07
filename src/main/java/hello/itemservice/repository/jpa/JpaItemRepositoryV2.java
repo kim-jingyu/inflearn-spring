fcda7d83f13fe5f78c0ab1c1e8a5ec54780e8c0f
jingyu9804 <swc05161@gmail.com>
2023-03-07 21:57:22 +0900

    feat(itemservice-db) : querydsl 적용

--- src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV2.java
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV2.java
@@ -12,7 +12,10 @@ import org.springframework.util.StringUtils;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+/**
+ * 스프링 데이터 JPA 적용
+ */
+//@Repository
 @Transactional
 @RequiredArgsConstructor
 public class JpaItemRepositoryV2 implements ItemRepository {
