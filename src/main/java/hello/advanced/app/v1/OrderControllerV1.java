3a6e35969919607428e5f7203a064cee2664005a
jingyu9804 <swc05161@gmail.com>
2023-06-06 18:10:06 +0900

    feat(advanced): 로그 추적기 V1 - 주문 기능에 적용

--- src/main/java/hello/advanced/app/v1/OrderControllerV1.java
+++ src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -1,18 +1,30 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class OrderControllerV1 {
     private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
 
     @GetMapping("/v1/request")
     public String request(@RequestParam String itemId) {
-        orderService.orderItem(itemId);
-        return "ok";
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()"); // 로그를 시작할 때, 메시지 이름으로 컨트롤러 이름 + 메서드 이름
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;    // 예외를 꼭 다시 던져주어야 한다. 그렇지 않으면 여기서 예외를 먹어버리고, 이후에 정상 흐름으로 동작한다.
+        }
     }
 }
