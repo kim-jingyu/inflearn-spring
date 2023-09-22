7f20efd7e820b08a23cc14a1cda3177f5bcfe35b
JingyuKim <swc05161@gmail.com>
2023-09-22 18:00:32 +0900

    feat: 템플릿 콜백 패턴

---
+++ advanced/src/main/java/hello/advanced/trace/callback/TraceTemplate.java
@@ -0,0 +1,13 @@
+package hello.advanced.trace.callback;
+
+import hello.advanced.trace.logtrace.LogTrace;
+
+public class TraceTemplate {
+    private final LogTrace trace;
+
+    public TraceTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+
+}
---
+++ advanced/src/test/java/hello/advanced/trace/strategy/code/Callback.java
@@ -0,0 +1,5 @@
+package hello.advanced.trace.strategy.code;
+
+public interface Callback {
+    void call();
+}
