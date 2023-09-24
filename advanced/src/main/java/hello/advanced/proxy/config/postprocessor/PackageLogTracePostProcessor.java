0c93d7de9825400367f8f414cd07d9af909fbe01
JingyuKim <swc05161@gmail.com>
2023-09-24 19:29:12 +0900

    feat: 패키지 기반 로그추적기 빈 후처리기 생성

---
+++ advanced/src/main/java/hello/advanced/proxy/config/postprocessor/PackageLogTracePostProcessor.java
@@ -0,0 +1,9 @@
+package hello.advanced.proxy.config.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@Slf4j
+public class PackageLogTracePostProcessor implements BeanPostProcessor {
+
+}
