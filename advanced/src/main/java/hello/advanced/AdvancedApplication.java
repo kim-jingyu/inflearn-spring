a57db833a0310446e5f450a24da4eee01653110e
kim-jingyu <swc05161@gmail.com>
2023-09-26 02:36:59 +0900

    feat: 스프링 AOP 빈 후처리기 적용

--- advanced/src/main/java/hello/advanced/AdvancedApplication.java
+++ advanced/src/main/java/hello/advanced/AdvancedApplication.java
@@ -2,13 +2,13 @@ package hello.advanced;
 
 import hello.advanced.ch0_1_2.trace.logtrace.LogTrace;
 import hello.advanced.ch0_1_2.trace.logtrace.ThreadLocalLogTrace;
-import hello.advanced.proxy.config.postprocessor.BeanPostProcessorConfig;
+import hello.advanced.proxy.config.autoproxy.AutoProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app.componentscan")
 public class AdvancedApplication {
 
