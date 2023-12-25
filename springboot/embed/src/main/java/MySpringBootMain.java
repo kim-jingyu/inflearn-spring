40e7f2755af2c3c06fd7b9605b8977a87fa1e40e
kim-jingyu <swc05161@gmail.com>
2023-12-25 23:00:00 +0900

    fix: 스프링 부트 만들기

--- springboot/embed/src/main/java/MySpringBootMain.java
+++ springboot/embed/src/main/java/MySpringBootMain.java
@@ -5,7 +5,6 @@ import boot.MySpringBootApplication;
 public class MySpringBootMain {
     public static void main(String[] args) {
         System.out.println("MySpringBootMain.main");
-
         MySpringApplication.run(MySpringBootMain.class, args);
     }
 }
--- springboot/embed/src/main/java/boot/MySpringApplication.java
+++ springboot/embed/src/main/java/boot/MySpringApplication.java
@@ -6,13 +6,13 @@ import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import spring.HelloConfig;
 
 import java.util.List;
 
 public class MySpringApplication {
-    public static void run(Class configClass, String[] args) {
-        System.out.println("MySpringApplication.run args = " + List.of(args));
+    public static void run(Class configclass, String[] args) {
+        System.out.println("MySpringApplication.run");
+        System.out.println("List.of(args) = " + List.of(args));
 
         // 톰캣 설정
         Tomcat tomcat = new Tomcat();
@@ -20,18 +20,17 @@ public class MySpringApplication {
         connector.setPort(8080);
         tomcat.setConnector(connector);
 
-        // 스프링 컨테이너 설정
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-        applicationContext.register(configClass);
+        // 스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(configclass);
 
         // 스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(appContext);
 
         // 디스패처 서블릿 등록
         Context context = tomcat.addContext("", "/");
         tomcat.addServlet("", "dispatcher", dispatcherServlet);
         context.addServletMappingDecoded("/", "dispatcher");
-
         try {
             tomcat.start();
         } catch (LifecycleException e) {
