07ce70c477f0e9cb5f53a8c830e3fd27b2534b11
kim-jingyu <swc05161@gmail.com>
2023-12-25 19:24:20 +0900

    chore: 주석 수정

--- springboot/embed/src/main/java/embed/EmbedTomcatServletMain.java
+++ springboot/embed/src/main/java/embed/EmbedTomcatServletMain.java
@@ -10,13 +10,13 @@ public class EmbedTomcatServletMain {
     public static void main(String[] args) throws LifecycleException {
         System.out.println("EmbedTomcatServletMain.main");
 
-        // 톰캣 설정
+        // 내장 톰캣 설정
         Tomcat tomcat = new Tomcat();
         Connector connector = new Connector();
         connector.setPort(8080);
         tomcat.setConnector(connector);
 
-        // 서블릿 등록
+        // Hello 서블릿 등록
         Context context = tomcat.addContext("", "/");
         tomcat.addServlet("", "helloServlet", new HelloServlet());
         context.addServletMappingDecoded("/hello-servlet", "helloServlet");
