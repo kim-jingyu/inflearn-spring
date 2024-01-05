34f7616970ef9f7cefdd56af31f10ef0b273a7bb
kim-jingyu <swc05161@gmail.com>
2024-01-05 17:14:02 +0900

    feat: 트랜잭션 - 트랜잭션 템플릿

---
+++ jdbc2/src/main/java/hello/jdbc2/service/MemberServiceV3_2.java
@@ -0,0 +1,49 @@
+package hello.jdbc2.service;
+
+import hello.jdbc2.domain.Member;
+import hello.jdbc2.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+    private final TransactionTemplate transactionTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        transactionTemplate.executeWithoutResult((status) -> {
+            try {
+                // 비즈니스 로직
+                businessLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void businessLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
